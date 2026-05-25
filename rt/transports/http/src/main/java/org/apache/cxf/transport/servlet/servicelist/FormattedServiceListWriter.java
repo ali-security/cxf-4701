71c6a1c65d09d0c2c251a9e40ef3510b40ec382f
Tal Samucha <tal.s@sealsecurity.io>
2026-05-25 16:38:38 +0300

    CVE-2019-17573

--- rt/transports/http/src/main/java/org/apache/cxf/transport/servlet/servicelist/FormattedServiceListWriter.java
+++ rt/transports/http/src/main/java/org/apache/cxf/transport/servlet/servicelist/FormattedServiceListWriter.java
@@ -24,6 +24,7 @@ import java.util.Map;
 
 import org.apache.cxf.service.model.OperationInfo;
 import org.apache.cxf.transport.AbstractDestination;
+import org.apache.cxf.transport.commons_text.StringEscapeUtils;
 
 public class FormattedServiceListWriter implements ServiceListWriter {
     private String styleSheetPath;
@@ -103,8 +104,8 @@ public class FormattedServiceListWriter implements ServiceListWriter {
         }
         writer.write("</ul>");
         writer.write("</td><td>");
-        
-        
+
+        absoluteURL = StringEscapeUtils.escapeHtml4(absoluteURL);
         writer.write("<span class=\"field\">Endpoint address:</span> " + "<span class=\"value\">"
                      + absoluteURL + "</span>");
         writer.write("<br/><span class=\"field\">WSDL :</span> " + "<a href=\"" + absoluteURL
@@ -153,7 +154,8 @@ public class FormattedServiceListWriter implements ServiceListWriter {
         if (absoluteURL == null) {
             return;
         }
-        
+        absoluteURL = StringEscapeUtils.escapeHtml4(absoluteURL);
+
         writer.write("<tr><td>");
         writer.write("<span class=\"field\">Endpoint address:</span> " + "<span class=\"value\">"
                      + absoluteURL + "</span>");
@@ -166,6 +168,7 @@ public class FormattedServiceListWriter implements ServiceListWriter {
     private static void addAtomLinkIfNeeded(String address, Map<String, String> extMap, PrintWriter pw) {
         String atomAddress = getExtensionEndpointAddress(address, extMap);
         if (atomAddress != null) {
+            atomAddress = StringEscapeUtils.escapeHtml4(atomAddress);
             pw.write("<br/><span class=\"field\">Atom Log Feed :</span> " + "<a href=\"" + atomAddress
                      + "\">" + atomAddress + "</a>");
         }
